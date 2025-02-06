10c06ed85a88b579cc43a648e21b98380db4125d
André Rocha <62674960+AnkhCorp@users.noreply.github.com>
2025-02-06 12:04:12 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,95 @@
+var textarea = $('.term');
+
+var i = 0;
+var count = 0;
+
+var start = 'ankhcorp.me'
+var data = [
+  './ankhcorp.me',
+  'Ankh',
+  '********',
+  'Yes\n',
+  'andre-rocha-',
+  '@wicked1',
+  '@coldx',
+  'andrewilliam@live.com\n\n',
+]
+
+var info = [
+  "Please state the user you would like to add > ",
+  "Please set the password for 'Ankh' > ",
+  "Shall we create a home directory for 'Ankh' > ",
+  "Directory Created, time for your socials!\n\nPlease state your Linkedin username, if none put 'N/A' > ",
+  "Please state your TryHackMe username, if none put 'N/A' > ",
+  "Please state your Blog username, if none put 'N/A' > ",
+  "Please state your Email, if none put 'N/A' > ",
+];
+
+runner(start)
+ 
+function runner(text) {
+  if (text) textarea.append(`<span style="color:rgb(255, 94, 102);">` + text.charAt(i) + `</span>`)
+
+  i++;
+  setTimeout(
+    function() {
+      if (i < text.length) {
+        runner(text);
+      } else {
+        // textarea.append("</span>")
+        textarea.append("<br>")
+        i = 0;
+        textarea.append(info[count])
+        count++;
+        runner(data[count])
+        if (count >= data.length) {
+          setTimeout(function() {feedbacker();}, 650);
+        }
+      }
+    }, Math.floor(Math.random() * 80) + 55)
+}
+
+var count = 0;
+var time = 1;
+
+function feedbacker() {
+  textarea.append("[" + count / 1000 + "] " + output[i] + "<br>");
+  if (time % 2 == 0) {
+    i++;
+    textarea.append("[" + count / 1000 + "] " + output[i] + "<br>");
+  }
+  if (time == 3) {
+    i++;
+    textarea.append("[" + count / 1000 + "] " + output[i] + "<br>");
+    i++;
+    textarea.append("[" + count / 1000 + "] " + output[i] + "<br>");
+    i++;
+    textarea.append("[" + count / 1000 + "] " + output[i] + "<br>");
+  }
+  window.scrollTo(0, document.body.scrollHeight);  
+  i++;
+  time = Math.floor(Math.random() * 4) + 1;
+  count += time;
+  setTimeout(
+    function() {
+      if (i < output.length - 2)
+        feedbacker();
+      else {
+        textarea.append("<br>Initialising...<br>");
+        setTimeout(function() {$(".load").fadeOut(2000);}, 1000);
+      }
+    },time);
+}
+
+var output = [
+  "\nInformation complete! Initializing database.",
+  "debug: Creating user account 'Ankh'",
+  "debug: Setting user account password",
+  "debug: Creating user home directory, /home/Ankh",
+  "debug: Granting sudo privileges to user 'Ankh'",
+  "debug: Setting telegram account name 'andre-rocha-'",
+  "debug: Setting Doxbin username '@wicked1'",
+  "debug: Setting Breachforums username '@coldx'",
+  "debug: Setting email address 'andrewilliam@live.com'",
+  "debug: Account initialized!"
+];
